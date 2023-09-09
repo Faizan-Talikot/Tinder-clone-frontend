ebda7897f69cee7570ce8bec5bcfc51f006d4058
Faizan-Talikot <97731892+Faizan-Talikot@users.noreply.github.com>
2023-09-09 22:08:11 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Nav from './Nav'
+
+const authToken = true
+
+const handleClick = ()=>{
+    console.log('clicked')
+}
+
+const Home = () => {
+  return (
+    <>
+    <Nav/>
+    <div className='home'>
+      <h1>Swipe Right©</h1>
+      <button className='primary-button' onClick={handleClick}>
+        {authToken ? 'Signout':'Create Account'}
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+     <div className='logo-container'>
+      <img className='logo' src=''/>
+     </div>
+    </nav>
+  )
+}
+
+export default Nav
