070f2810ca8919db2c5e062d61ec80d474b6adaa
Faizan-Talikot <97731892+Faizan-Talikot@users.noreply.github.com>
2023-09-10 17:50:08 +0530

    faizan commit 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import Nav from './Nav'
+import React from "react";
+import Nav from "./Nav";
+import AuthModal from "./AuthModal";
+import { useState } from "react";
 
 const Home = () => {
+  const [showModal,setShowModal] = useState(false);
+  const [isSignUp,setIsSignUp] = useState(true)
 
-const authToken = true
+  const authToken = false;
 
-const handleClick = ()=>{
-    console.log('clicked')
-}
-
+  const handleClick = () => {
+    console.log("clicked");
+    setShowModal(true)
+    setIsSignUp(true)
+  };
 
   return (
     <>
-    <div className="overlay">
-    <Nav minimal={false} authToken={authToken}/>
-    <div className='home'>
-      <h1>Swipe Right©</h1>
-      <button className='primary-button' onClick={handleClick}>
-        {authToken ? 'Signout':'Create Account'}
-      </button>
-    </div>
-    </div></>
-  )
-}
+      <div className="overlay">
+        <Nav minimal={false}
+             authToken={authToken} 
+             setShowModal={setShowModal} 
+             showModal={showModal}
+             setIsSignUp={setIsSignUp} />
+        <div className="home">
+          <h1 className="primary-title">Swipe Right©</h1>
+          <button className="primary-button" onClick={handleClick}>
+            {authToken ? "Signout" : "Create Account"}
+          </button>
 
-export default Home
+          {showModal &&(
+            <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,15 +3,19 @@ import whitelogo from '../images/tinder_logo_white.png'
 import colorlogo from '../images/color-logo-tinder.png'
 
 
-const Nav = ({minimal,authToken}) => {
+const Nav = ({minimal,authToken,setShowModal,showModal,setIsSignUp}) => {
 
+  const handleClick = ()=>{
+    setShowModal(true)
+    setIsSignUp(false)
+  }
  
   return (
     <nav>
      <div className='logo-container'>
           <img className='logo' src={minimal ? colorlogo:whitelogo}/>
      </div>
-     {!authToken && !minimal && <button className="nav-button"> Log in </button>}
+     {!authToken && !minimal && <button className="nav-button" onClick={handleClick} disabled={showModal}> Log in </button>}
     </nav>
   )
 }
