539ad243d69f9dfb2e5ae85991405ac54edeba8b
Faizan-Talikot <97731892+Faizan-Talikot@users.noreply.github.com>
2023-09-15 22:18:01 +0530

    faizan commit 3(dashboard complete)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import ChatContainer from './ChatContainer'
+import TinderCard from 'react-tinder-card'
+import { useState } from 'react'
 
 const Dashboard = () => {
+  const characters = [
+    {
+      name: 'Richard Hendricks',
+      url: 'https://imgur.com/Q9WPlWA.jpg'
+    },
+    {
+      name: 'Erlich Bachman',
+      url: 'https://imgur.com/Q9WPlWA.jpg'
+    },
+    {
+      name: 'Monica Hall',
+      url: 'https://imgur.com/Q9WPlWA.jpg'
+    },
+    {
+      name: 'Jared Dunn',
+      url: 'https://imgur.com/Q9WPlWA.jpg'
+    },
+    {
+      name: 'Dinesh Chugtai',
+      url: 'https://imgur.com/Q9WPlWA.jpg'
+    }
+  ]
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
   return (
-    <div>
-        Dashboard
+    <div className='dashboard'>
+      <ChatContainer/>
+      <div className="swipe-container">
+        <div className="card-container">
+          
+        {characters.map((character) =>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        )}
+
+        </div>
+      <div className="swipe-info">
+        {/* {lastDirection?<p>You swiped {lastDirection}</p>:<p/>} */}
+      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+      </div>
+      </div>
     </div>
   )
 }
